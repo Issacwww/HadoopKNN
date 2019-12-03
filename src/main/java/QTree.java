ed9125b63e8dd1ab858cf7141f2a41917e596d12
LUZ29 <LUZ29@pitt.edu>
2019-12-03 16:03:34 -0500

    point&node

--- src/main/java/QTree.java
+++ src/main/java/QTree.java
@@ -1,66 +1,6 @@
 import java.util.ArrayList;
 import java.util.HashMap;
-class Point{
-    double x;
-    double y;
-    public Point(double x, double y){
-        this.x = x;
-        this.y = y;
-    }
 
-    @Override
-    public String toString() {
-        return "("+x+","+y+")";
-    }
-}
-class Node {
-    double x;
-    double y;
-    double sideLen;
-    String id;
-    private Node parent;
-    ArrayList<Point> points;
-    ArrayList<Node> children;
-    public Node(){}
-    public Node(double x0, double y0, double sideLen, Node parent, String id){
-        this.x = x0;
-        this.y = y0;
-        this.sideLen = sideLen;
-        this.parent = parent;
-        this.id = id;
-        this.points = new ArrayList<>();
-        this.children = new ArrayList<>();
-    }
-
-    public Node getParent() {
-        return parent;
-    }
-
-    public int getSum() {
-        return this.points.size();
-    }
-
-    @Override
-    public String toString() {
-        StringBuilder childrenStr = new StringBuilder("[ ");
-        for(Node child :children)
-            childrenStr.append("'" + child.id + "', ");
-        childrenStr.append("]");
-        StringBuilder pointsStr = new StringBuilder("[");
-        for(Point point :points)
-            pointsStr.append(point);
-        pointsStr.append("]");
-        return "Node {" +
-                "x=" + x +
-                ", y=" + y +
-                ", sideLen=" + sideLen +
-                ", id='" + id + '\'' +
-                ", parent='" + parent.id + '\'' +
-                ", children=" + childrenStr.toString() +
-                ", points=" + pointsStr.toString() +
-                "}";
-    }
-}
 public class QTree {
     int levels;
     double sideLen;
